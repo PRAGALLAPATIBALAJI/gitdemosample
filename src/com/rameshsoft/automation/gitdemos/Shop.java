eb32a461f5dceb9d9f34be13937bfd16f454de59
PRAGALLAPATIBALABALAJI <balabalajipragallapati6@gmail.com>
2022-09-22 11:18:57 +0530

    added shop project\

---
+++ src/com/rameshsoft/automation/gitdemos/Shop.java
@@ -0,0 +1,7 @@
+package com.rameshsoft.automation.gitdemos;
+
+public class Shop {
+public static void main(String[] args) {
+	System.out.println("SRI VIJAYA BALAJI FANCY AND GENERAL STORES");
+}
+}
